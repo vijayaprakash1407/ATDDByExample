8cb669eda00de51f01bb482c4249da8280053a67
Chase <chaseajohnson@gmail.com>
2015-02-09 16:38:22 +1100

    Cucumber Parking to include short term and economy options

--- Cucumber/Parking/features/step_definitions/Valet_steps.rb
+++ Cucumber/Parking/features/step_definitions/Valet_steps.rb
@@ -1,8 +1,8 @@
-When /^I park my car in the Valet Parking Lot for (.*)$/ do |duration|
-  $parkcalc.select('Valet Parking')
+When /^I park my car in the (.*) Lot for (.*)$/ do | parking_lot, duration |
+  $parkcalc.select(parking_lot)
   $parkcalc.enter_parking_duration(duration)
 end
 
-Then /^I will have to pay (.*)$/ do |price|
+Then /^I will have to pay (.*)$/ do | price |
   $parkcalc.parking_costs.should == price
 end
